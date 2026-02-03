062e68cf0913a23b8290e8a4cadcded5c3ae2fbb
phillipwachsmann <phillip.wachsmann@bwz-rappi.ch>
2026-02-03 13:58:02 +0100

    dockerstart auf disabled gamerules version geändert

--- start-docker.bat
+++ start-docker.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Starting Docker Container...
-docker-compose -f C:\Users\phill\Desktop\Module\M231\edu-pixelboard-no-auth\docker-compose.yml up -d
+docker-compose -f C:\Users\phill\Desktop\Module\M231\edu-pixelboard-disabled-game-rules\docker-compose.yml up -d
 
 echo Setting environment for Docker API...
 set API_URL=http://localhost:5085
@@ -8,3 +8,4 @@ set NEXT_PUBLIC_API_URL=http://localhost:3000
 
 echo Starting Next.js in development mode...
 next dev
+:: docker-compose -f C:\Users\phill\Desktop\Module\M231\edu-pixelboard-no-auth\docker-compose.yml up -d
